77854e625e9161b0334a26a61ac0440403cc6409
zuoerdong <left2east@163.com>
2015-06-21 10:44:24 +0800

    update YunPian

--- YunPian.php
+++ YunPian.php
@@ -40,10 +40,11 @@ function _send($url, array $params = array()) {
     
     return true;
 }
+header ( "Content-Type: text/html; charset=utf-8" );
 $url = 'http://yunpian.com/v1/sms/send.json';
 $apikey = '01b849d086b2e1fd4fe394cbadd73060';
 $templates = array (
-        '【掌上链家】掌上链家提醒您：您修改密码的手机验证码为#verify_code#，请填写验证码完成密码修改。',
+        '【掌上链家】掌上链家提醒您：您修改密码的手机验证码为#mycode#，请填写验证码完成密码修改。',
         '【链家网】链家网提醒您：您的手机验证码为#mycode#，请在#myttl#分钟内填写验证码完成操作。',
         '【链家网】链家网提醒您：您注册的手机验证码为#mycode#，有效期#myttl#分钟，请在注册页面填写验证码完成注册。',
         '【链家网】链家网提醒您：您登录的手机验证码为#mycode#，有效期#myttl#分钟，请在登录页面填写验证码完成登录。',
@@ -57,20 +58,20 @@ $templates = array (
 );
 $mycode = '1234';
 $t = 10;
-header ( "Content-Type: text/html; charset=utf-8" );
 $i = $_REQUEST ['num'];
 if (empty ( $templates [$i] )) {
     echo 'end';
     die ();
 }
-$str1 = str_replace ( '#mycode#', $mycode, $templates [$i] );
-$str2 = str_replace ( '#myttl#', $t, $str1 );
-$mobile1 ='18600045277';
+$arrParam = array( '#mycode#' => '1234', '#myttl#' => '10');
+$text = strtr( $templates [$i] , $arrParam );
+
+//$mobile1 ='18600045277';
 $mobile2 = '18310426754';
 $param = array (
         'apikey' => $apikey,
-        'mobile' => $mobile1,
-        'text' => $str2
+        'mobile' => $mobile2,
+        'text' => $text
 );
 var_dump ( $str2 );
 echo '<br />';
